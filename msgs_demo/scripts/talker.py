33e869abed321bdf11bdcc7385ca8f106bc87fd7
yyh-create <61340379+yyh-create@users.noreply.github.com>
2020-03-13 10:34:46 +0800

    Update talker.py
    second edition

--- msgs_demo/scripts/talker.py
+++ msgs_demo/scripts/talker.py
@@ -4,13 +4,15 @@ import rospy
 from std_msgs.msg import String
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub = rospy.Publisher('chatter', String, queue_size=20)
     rospy.init_node('talker', anonymous=True)  
     rate = rospy.Rate(10) # 10times/s
+    i=1
     while not rospy.is_shutdown():
-        hello_str = "1710675 "   
+        hello_str = "1710675 %s" % i   
         rospy.loginfo(hello_str)  
         pub.publish(hello_str)
+        i=i+1
         rate.sleep()
 
 if __name__ == '__main__':
